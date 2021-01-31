c5c38447e6f755f67926394de469c20ec4536dcb
Aseed Khokhar <71330292+aseedk@users.noreply.github.com>
2021-01-31 22:46:31 +0500

    Initial commit

---
+++ Practice/Practice 1.py
@@ -0,0 +1,12 @@
+a = 2
+b = "asadsaf"
+c = 3.2
+print(type(a))
+print(type(b))
+print(type(c))
+d = b.replace("a", "f")
+print(d)
+print(b[3:])
+a = {"name": ["ahmed", "ali", "ashir"], "age": [21,24]}
+print(a["name"])
+print(a.values())
